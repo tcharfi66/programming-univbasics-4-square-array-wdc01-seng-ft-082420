02fe908c1aad1d7ca15eb360e0e8f8797182b58c
Tej Charfi <charfi@usc.edu>
2020-08-16 16:57:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,13 @@
 
 def square_array(array)
 
-sq = []
-ctr = 0
-while ctr< array.length do 
-    sq.push((array[ctr] ** 2))
-    ctr +=1
-end
-
-sq
+  sq = []
+  ctr = 0
+  while ctr< array.length do 
+      sq.push((array[ctr] ** 2))
+      ctr +=1
+  end
+  
+  sq
 
 end
